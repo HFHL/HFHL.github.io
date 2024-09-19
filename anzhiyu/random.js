2364575145ca6e1764bf3424686382357d3b4dc1
hehailin <feiyuzi51@gmail.com>
2024-09-19 17:36:13 +0800

    Site updated: 2024-09-19 17:36:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2077/07/07/board/","2024/05/09/diary0/","2024/07/12/dream/","2024/04/19/hello-world/","2024/05/09/吾心于我即一王国/","2024/06/02/flower/","2024/05/20/换锁心得与技巧/","2024/05/27/这个蘑菇可以吃吗/"];function toRandomPost(){
+var posts=["2024/07/12/dream/","2077/07/07/board/","2024/04/19/hello-world/","2024/05/09/diary0/","2024/05/27/这个蘑菇可以吃吗/","2024/05/09/吾心于我即一王国/","2024/05/20/换锁心得与技巧/","2024/06/02/flower/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
